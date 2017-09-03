7fd9ff70750f72422837ea5f73e2b505e37d7c28
nikzsaz <mr.nirajjee@gmail.com>
2017-09-03 13:30:23 +0530

    Attr, class, text, html and val uses

---
+++ example2.js
@@ -0,0 +1,35 @@
+$(function(){
+    // gimme the what is the name of class of p tag that is first
+    // alert($('p:first').attr('class'));
+    //set the new clas
+    //$('p:first').attr('class', 'not-lead')
+
+    //image swap(now sorce is another image)
+    //$('img').attr('src','img2.jpg');
+    //function to swap the images 
+    // $('img').delay(300).fadeOut(300,function(){
+    //   $(this).attr('src','img2.jpg').fadeIn(500);  
+    // });
+
+    //class methods
+    // to check of the last para has lead class
+    //alert($('p:last').hasClass('lead'));
+    //alert($('p:first').hasClass('lead'));
+    // to add and remove the class in same line
+    //$('p:first').addClass('blue').removeClass('lead');
+    //$('p').toggleClass('blue').removClass('lead');
+
+    //content moethods
+    // to set the text methods
+    // $('p:first').text("Heeeeeelo Worlddddd");
+    // to see all the text of the particular tag
+    // /alert($('p:first').text());
+    // this will show the text as it is
+    //$('p:first').text("Hey there <em>buddy</em>");
+    // this is for the html
+    //$('p:first').html("Hey there <em>buddy</em>");
+    //to get the input from the input type
+    //alert($('input').val());
+    // to set the value of the 
+    //$('input').val("Hello guyyyyyyyys");
+})
