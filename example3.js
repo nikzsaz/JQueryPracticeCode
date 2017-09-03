3215e734530affbe5ef69f9318641fb09cae7e57
nikzsaz <mr.nirajjee@gmail.com>
2017-09-03 13:47:56 +0530

    Event Binding and dom Traversal

---
+++ example3.js
@@ -0,0 +1,29 @@
+$(function(){
+    //dom traveral
+    //$('h2').css("border","4px solid red");
+    //$('h2').children().css("border","4px solid red");
+    // $('h2').parents().css("border","4px solid red");
+    // $('h2').parents('section').css("border","4px solid red");
+    //$('h2').siblings().css("border","4px solid red");
+    // $('h2').siblings('p:first').css("border","4px solid red");
+    //$('h2').parents('section').siblings('header').css("border","4px solid red");
+     //$('h2').parents('section').siblings('header').children().css("border","4px solid red");
+
+    //event binding
+    // $('h2').bind('click',function(){
+    //     $(this).toggleClass('blue');
+    // // });
+
+    // $('h2').click(function(){
+    //         $(this).toggleClass('blue');
+    //     });
+    
+    // $('h2').hover(function(){
+    //         $(this).toggleClass('blue');
+    //  });
+
+    // $('html').keypress(function(){
+    //          $(this).toggleClass('blue');
+    //  });
+
+});
